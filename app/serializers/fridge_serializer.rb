f306d97025edfe5f840935bd7c67cb3727f7c841
renburnett <renardburnett@gmail.com>
2019-11-22 09:53:39 -0800

    tweaked serializers

--- app/serializers/fridge_serializer.rb
+++ app/serializers/fridge_serializer.rb
@@ -1,5 +1,3 @@
 class FridgeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :user, :food_capacity, :drink_capacity, :total_items_value
+  attributes :id, :name, :user_id, :food_items, :food_capacity, :drink_capacity, :total_items_value
 end
-
-
