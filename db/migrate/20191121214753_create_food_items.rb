da5b63318b122a3a0b88fc920a9bcb3c5be8a1ae
renburnett <renardburnett@gmail.com>
2019-11-25 15:04:45 -0800

    modified seeds and schema to include isProcessd? boolean

--- db/migrate/20191121214753_create_food_items.rb
+++ db/migrate/20191121214753_create_food_items.rb
@@ -3,6 +3,7 @@ class CreateFoodItems < ActiveRecord::Migration[6.0]
     create_table :food_items do |t|
       t.string :name
       t.boolean :is_drink
+      t.boolean :is_processed
       t.float :price
       t.datetime :expiration_date
       t.references :fridge, null: false, foreign_key: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,66 +7,103 @@ ron = User.create(name: "ron", email: "r@r.com", password: "123", budget: 132.44
 don = User.create(name: "don", email: "d@d.com", password: "123", budget: 666.00)
 
 kitchenFridge = Fridge.create(
-                                name: "Kitchen Fridge", 
-                                user: don,
-                                food_capacity: 8,
-                                drink_capacity: 10, 
-                                is_full: false, 
-                                total_items_value: 128.92
-                                )
+  name: "Kitchen Fridge", 
+  user: don,
+  food_capacity: 8,
+  drink_capacity: 10, 
+  is_full: false, 
+  total_items_value: 128.92
+)
 
 basementFridge = Fridge.create(
-                              name: "Basement Fridge", 
-                              user: ron,
-                              food_capacity: 10,
-                              drink_capacity: 12, 
-                              is_full: false, 
-                              total_items_value: 188.41
-                              )
+  name: "Basement Fridge", 
+  user: ron,
+  food_capacity: 10,
+  drink_capacity: 12, 
+  is_full: false, 
+  total_items_value: 188.41
+)
 
+garageFridge = Fridge.create(
+  name: "Garage Fridge", 
+  user: ron,
+  food_capacity: 4,
+  drink_capacity: 5, 
+  is_full: false,
+  total_items_value: 88.41
+)
 
 # Seed basement fridge tings
 basementFridge.food_items.create(
-                                  name: "pear",
-                                  is_drink: false,
-                                  price: 1.32,
-                                  expiration_date: DateTime.new(2019, 11, 25, 9, 23, 43)
-                                )
+  name: "pear",
+  is_drink: false,
+  is_processed: false,
+  price: 1.32,
+  expiration_date: DateTime.new(2019, 11, 25, 9, 23, 43)
+)
 
 basementFridge.food_items.create(
-                                  name: "lunchable",
-                                  is_drink: false,
-                                  price: 3.63,
-                                  expiration_date: DateTime.new(2019, 11, 26, 11, 50, 13)
-                                )
+  name: "lunchable",
+  is_drink: false,
+  is_processed: true,
+  price: 3.63,
+  expiration_date: DateTime.new(2019, 11, 26, 11, 50, 13)
+)
 
 basementFridge.food_items.create(
-                                  name: "apple juice",
-                                  is_drink: true,
-                                  price: 3.80,
-                                  expiration_date: DateTime.new(2019, 11, 24, 12, 16, 59)
-                                )
-
+  name: "apple juice",
+  is_drink: true,
+  is_processed: true,
+  price: 3.80,
+  expiration_date: DateTime.new(2019, 11, 24, 12, 16, 59)
+)
 
 # Seed kitchen fridge tings
 kitchenFridge.food_items.create(
-                                  name: "1lb of potatoes",
-                                  is_drink: false,
-                                  price: 6.04,
-                                  expiration_date: DateTime.new(2019, 11, 25, 9, 23, 43)
-                                )
-
-
-kitchenFridge.food_items.create(
-                                  name: "beer",
-                                  is_drink: true,
-                                  price: 12.93,
-                                  expiration_date: DateTime.new(2020, 11, 26, 12, 50, 55)
-                                )
+  name: "1lb of potatoes",
+  is_drink: false,
+  is_processed: false,
+  price: 6.04,
+  expiration_date: DateTime.new(2019, 11, 25, 9, 23, 43)
+)
 
 kitchenFridge.food_items.create(
-                                  name: "gogurt box",
-                                  is_drink: true,
-                                  price: 8.75,
-                                  expiration_date: DateTime.new(2020, 1, 24, 12, 16, 59)
-                                )
+  name: "soylent green 12pk",
+  is_drink: true,
+  is_processed: true,
+  price: 12.93,
+  expiration_date: DateTime.new(2020, 11, 26, 12, 50, 55)
+)
+
+kitchenFridge.food_items.create(
+  name: "gogurt multipack",
+  is_drink: true,
+  is_processed: true,
+  price: 8.75,
+  expiration_date: DateTime.new(2020, 1, 24, 12, 16, 59)
+)
+
+# Seed garage fridge tings
+garageFridge.food_items.create(
+      name: "salami",
+      is_drink: false,
+      is_processed: true,
+      price: 6.78,
+      expiration_date: DateTime.new(2019, 12, 29, 1, 33, 49)
+    )
+
+garageFridge.food_items.create(
+      name: "ice cream",
+      is_drink: false,
+      is_processed: true,
+      price: 5.37,
+      expiration_date: DateTime.new(2020, 02, 15, 19, 52, 43)
+    )
+
+garageFridge.food_items.create(
+      name: "wine",
+      is_drink: true,
+      is_processed: true,
+      price: 10.61,
+      expiration_date: DateTime.new(2020, 03, 04, 23, 33, 31)
+    )
