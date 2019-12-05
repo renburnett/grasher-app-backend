e7e2d35640c729e19c8d86260c11484f677e588c
renburnett <renardburnett@gmail.com>
2019-12-04 17:51:21 -0800

    started setting up new-fridge-add-form

--- app/controllers/fridges_controller.rb
+++ app/controllers/fridges_controller.rb
@@ -1,5 +1,29 @@
 class FridgesController < ApplicationController
+  
   def index
     render json: Fridge.all
   end
+
+  def create
+    @fridge = Fridge.new(fridge_params)
+    @fridge.save
+    @fridge.user = @user
+
+    render json: @fridge
+  end
+
+  def destroy
+  
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def fridge_params
+    params.require(:fridge).permit(:name, :user_id, :food_capacity, :drink_capacity, :is_full, :total_items_value)
+  end
+
 end
