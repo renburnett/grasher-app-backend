5c163c91da3887e37ab2ef196f4ceaf01214692a
renburnett <renardburnett@gmail.com>
2019-12-15 15:51:31 -0800

    added users#create route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,13 @@ class UsersController < ApplicationController
     render json: @user
   end
 
+  def create
+    @user = User.new(user_params)
+    @user.save
+    
+    render json: @user
+  end
+
   private 
 
   def user_params
