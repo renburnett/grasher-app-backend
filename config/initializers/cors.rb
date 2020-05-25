449a73d01828348d9d588d9f16c9f2d0e25d73fd
renburnett <renardburnett@gmail.com>
2020-05-25 13:40:52 -0700

    removed cors restrictions

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins 'https://grasher-food-tracker-backend.herokuapp.com', 'http://localhost:3000', 'http://localhost:3001'
-    '*'
+    origins '*'
 
     resource '*',
       headers: :any,
